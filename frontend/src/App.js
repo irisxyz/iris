5244a16eda59b281de7591b541df615a5f4ea58b
unknown <ianmmah@gmail.com>
2022-03-21 15:44:09 -0700

    create-react-app@4.0.3 because I hate webpack 5, skeleton cra

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React. Find God.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
