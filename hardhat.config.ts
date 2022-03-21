21f9a0c671ac6100053d0b029dd8085f02a9d6ad
unknown <ianmmah@gmail.com>
2022-03-21 16:02:54 -0700

    Unpause task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ import "@nomiclabs/hardhat-etherscan";
 import glob from 'glob';
 import path from "path";
 
-const { API_URL, PRIVATE_KEY, PRIVATE_KEY2, PRIVATE_KEY3, POLYGONSCAN_API } = process.env;
+const { API_URL, PRIVATE_KEY, PRIVATE_KEY2, PRIVATE_KEY3, PRIVATE_KEY4, POLYGONSCAN_API } = process.env;
 
 if (!process.env.SKIP_LOAD) {
   glob.sync('./tasks/**/*.ts').forEach(function (file) {
@@ -34,7 +34,7 @@ export const networks = {
   hardhat: {},
   polygon_mumbai: {
     url: API_URL,
-    accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`, `0x${PRIVATE_KEY3}`]
+    accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`, `0x${PRIVATE_KEY3}`, `0x${PRIVATE_KEY4}`]
   }
 };
 export const spdxLicenseIdentifier = {
