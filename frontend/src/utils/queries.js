b5be48ff9f55242fa2727d90cb731a8476889dc5
unknown <ianmmah@gmail.com>
2022-03-23 01:25:15 -0700

    Compose

--- frontend/src/utils/queries.js
+++ frontend/src/utils/queries.js
@@ -80,3 +80,36 @@ query($request: ProfileQueryRequest!) {
   }
 }
 `;
+
+export const CREATE_POST_TYPED_DATA = gql`
+  mutation($request: CreatePublicPostRequest!) { 
+    createPostTypedData(request: $request) {
+      id
+      expiresAt
+      typedData {
+        types {
+          PostWithSig {
+            name
+            type
+          }
+        }
+      domain {
+        name
+        chainId
+        version
+        verifyingContract
+      }
+      value {
+        nonce
+        deadline
+        profileId
+        contentURI
+        collectModule
+        collectModuleData
+        referenceModule
+        referenceModuleData
+      }
+    }
+  }
+}
+`;
