0bc36cc95e0983dd0e36c9c4fecea655d09244bc
Stanford Lin <stanfordlin@outlook.com>
2022-03-30 01:27:41 -0700

    Renamed select file to select video

--- frontend/src/components/Compose.js
+++ frontend/src/components/Compose.js
@@ -420,7 +420,7 @@ const Compose = ({ wallet, profile, lensHub }) => {
                     </>
                         : <div class="file-input">
                             <FileInput type="file" id="file" class="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
-                            <CustomLabel for="file">Select File</CustomLabel>
+                            <CustomLabel for="file">Select Video</CustomLabel>
                         </div>}
                 </InputWrapper>
 
