f1c75d5fc2be3f462b6299a7069eeeda14926020
unknown <ianmmah@gmail.com>
2022-06-13 13:50:11 -0700

    rename logout icon

---
+++ frontend/src/assets/Logout.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const SVG = styled.svg`
+    path {
+        stroke: #747c90;
+        stroke-width: 3px;
+        transition: all 100ms ease-in-out;
+    }
+    transition: all 100ms ease-in-out;
+    &:hover {
+        cursor: pointer;
+        path {
+            stroke: #F28A56;
+        }
+    }
+    ${(p) => p.filled && `stroke: #F28A56;`}
+`;
+
+const Icon = ({ ...props }) => {
+    return (
+        <SVG {...props} width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21.75 10.75L27 16L21.75 21.25" stroke="#8247E5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M13 16H27" stroke="#8247E5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M13 27H6C5.73478 27 5.48043 26.8946 5.29289 26.7071C5.10536 26.5196 5 26.2652 5 26V6C5 5.73478 5.10536 5.48043 5.29289 5.29289C5.48043 5.10536 5.73478 5 6 5H13" stroke="#8247E5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        </SVG>
+    );
+};
+
+export default Icon;
