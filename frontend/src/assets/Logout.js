819f57a7868623f8d1ac7eb93b1e9747ef49b0eb
unknown <ianmmah@gmail.com>
2022-06-15 16:31:05 -0700

    Resolve react errors

--- frontend/src/assets/Logout.js
+++ frontend/src/assets/Logout.js
@@ -19,9 +19,9 @@ const SVG = styled.svg`
 const Icon = ({ ...props }) => {
     return (
         <SVG {...props} width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M21.75 10.75L27 16L21.75 21.25" stroke="#8247E5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M13 16H27" stroke="#8247E5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M13 27H6C5.73478 27 5.48043 26.8946 5.29289 26.7071C5.10536 26.5196 5 26.2652 5 26V6C5 5.73478 5.10536 5.48043 5.29289 5.29289C5.48043 5.10536 5.73478 5 6 5H13" stroke="#8247E5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M21.75 10.75L27 16L21.75 21.25" stroke="#8247E5" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M13 16H27" stroke="#8247E5" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M13 27H6C5.73478 27 5.48043 26.8946 5.29289 26.7071C5.10536 26.5196 5 26.2652 5 26V6C5 5.73478 5.10536 5.48043 5.29289 5.29289C5.48043 5.10536 5.73478 5 6 5H13" stroke="#8247E5" strokeLinecap="round" strokeLinejoin="round"/>
         </SVG>
     );
 };
