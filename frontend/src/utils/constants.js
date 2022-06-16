c7877cf7a2195be59471374055b07b9b3a4763fa
unknown <ianmmah@gmail.com>
2022-06-15 18:28:05 -0700

    Modify chain logic

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,16 +1,2 @@
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-export const CHAIN = () => {
-    // so you can override localhost
-    if (process.env.REACT_APP_CHAIN) {
-        return process.env.REACT_APP_CHAIN
-    } else {
-        switch(process.env.NODE_ENV) {
-            case 'development':
-                return 'mumbai';
-            case 'production':
-                return 'polygon';
-            default:
-                return 'mumbai';
-        } 
-    }
-}
+export const CHAIN = process.env.REACT_APP_CHAIN ? process.env.REACT_APP_CHAIN : process.env.NODE_ENV === 'production' ? 'polygon' : 'mumbai'
