488356dbc673bdd4cfb7c73f0e9d30a51722d489
unknown <vandyliu1999@gmail.com>
2022-08-08 02:52:00 -0700

    resorted to third party :(

--- frontend/src/assets/X.js
+++ frontend/src/assets/X.js
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-
 const Icon = ({...props }) => {
     return (
         <svg width="10" height="10" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -1,10 +1,7 @@
-import { useEffect, useState, useRef } from 'react'
 import styled from 'styled-components'
-import Play from '../assets/Play'
-import Pause from '../assets/Pause'
-import VolumeSpeaker from '../assets/VolumeSpeaker'
-import FullScreen from '../assets/FullScreen'
 import X from '../assets/X';
+import 'plyr-react/plyr.css'
+import Plyr from 'plyr-react'
 
 const CloseButton = styled.button`
     color: white;
@@ -24,216 +21,45 @@ const CloseButton = styled.button`
 const Container = styled.div`
     background-color: rgba(100, 100, 100, 0);
     display: flex;
+    justify-content: center;
     flex-direction: column;
     width: 100%;
-`;
-
-const VideoContainer = styled.div`
-    background-color: black;
-    display: flex;
-    ${p => p.fullscreened ? 'height: 100%' : 'height: 18em'};
-    aspect-ratio: 16 / 9;
     position: relative;
-    border-radius: 12px;
-`;
 
-const ControlsContainer = styled.div`
-    position: relative;
-`;
+    --plyr-color-main: ${p => p.theme.primary};
 
-const VideoPlayer = styled.video`
-    border-radius: 6px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    width: 100%;
-`;
-
-const Controls = styled.div`
-    position: absolute;
-    top: -35px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    background-color: rgba(0, 0, 0, 0.2);
-`;
-
-const TimeControls = styled.div`
-    display: flex;
-`
-
-const TimeText = styled.p`
-    color: white;
-`;
-
-const TimeProgressBarContainer = styled.div``
-
-const TimeProgressBar = styled.div`
-    width: 40%;
-    color: white;
-    background-color: red;
-`
-
-const VolumeControls = styled.div``;
-
-const Icon = styled.button`
-    :hover {
-        cursor: pointer;
+    .plyr {
+        background-color: rgba(0, 0, 0, 0);
+        border-radius: 6px;
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     }
 `;
-
-
-// https://blog.avneesh.tech/create-a-custom-video-player-in-react
-// https://dev.to/franciscomendes10866/how-to-create-a-video-player-in-react-40jj
-
-const convertSeconds = (e, includeHours) => {
-        const h = Math.floor(e / 3600).toString().padStart(2,'0'),
-              m = Math.floor(e % 3600 / 60).toString().padStart(2,'0'),
-              s = Math.floor(e % 60).toString().padStart(2,'0');
-        
-        return includeHours ? h + ':' + m + ':' + s : m + ':' + s;
-        //return `${h}:${m}:${s}`;
-    }
-
-
-const useVideoPlayer = (videoRef) => {
-    const [isPaused, setIsPaused] = useState(true);
-    const [volume, setVolume] = useState(100);
-    const [currentTime, setCurrentTime] = useState(0);
-    const [duration, setDuration] = useState(0);
-
-    const handleVideoProgress = (event) => {
-        const manualChange = Number(event.target.value);
-        videoRef.current.currentTime = (videoRef.current?.duration / 100) * manualChange;
-    };
-
-    useEffect(() => {
-        setDuration(videoRef.current?.duration);
-        if (isPaused) {
-            videoRef.current?.pause();
-        } else {
-            videoRef.current?.play();
-        }
-    }, [isPaused, videoRef])
-
-    const onTimeUpdate = () => {
-        const ref = videoRef.current;
-        if (ref) {
-            setDuration(ref.duration);
-            setCurrentTime(ref.currentTime);
-        }
-    }
-
-    return {
-        isPaused,
-        setIsPaused,
-        volume,
-        currentTime,
-        duration,
-        handleVideoProgress,
-        onTimeUpdate
-    };
-}
 
 const Video = ({src, hasCloseButton, closeButtonFn}) => {
-    const containerRef = useRef(null);
-    const videoRef = useRef(null);
-    const [fullscreened, setFullscreened] = useState(false);
-    const {
-        isPaused,
-        setIsPaused,
-        volume,
-        currentTime,
-        duration,
-        handleVideoProgress,
-        onTimeUpdate
-      } = useVideoPlayer(videoRef);
-
     const url = URL.createObjectURL(src);
-    
-    const conversionFn = (e) => convertSeconds(e, videoRef.current?.duration > 3600);
-
-    // Functions needed to deal with fullscreening and exiting fullscreen
-    const toggleFullScreen = () => {
-        if (!document.fullscreenElement) {
-            if (containerRef.current?.requestFullscreen) {
-              containerRef.current?.requestFullscreen();
-            } else if (containerRef.current?.msRequestFullscreen) {
-              containerRef.current?.msRequestFullscreen();
-            } else if (containerRef.current?.mozRequestFullScreen) {
-              containerRef.current?.mozRequestFullScreen();
-            } else if (containerRef.current?.webkitRequestFullscreen) {
-              containerRef.current?.webkitRequestFullscreen();
-            }
-            setFullscreened(true);
-        } else {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            }
-            setFullscreened(false);
-        }
-      };
-
-    const exitHandler = () => {
-          if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
-              setFullscreened(false);
-          }
-    }
-
-    useEffect(() => {
-        document.addEventListener('fullscreenchange', exitHandler);
-        document.addEventListener('webkitfullscreenchange', exitHandler);
-        document.addEventListener('mozfullscreenchange', exitHandler);
-        document.addEventListener('MSFullscreenChange', exitHandler);
-        return () => {
-            document.removeEventListener('fullscreenchange', exitHandler);
-            document.removeEventListener('webkitfullscreenchange', exitHandler);
-            document.removeEventListener('mozfullscreenchange', exitHandler);
-            document.removeEventListener('MSFullscreenChange', exitHandler);
-        }
-    })
 
     return (
-        <Container ref={containerRef}>
-            <VideoContainer fullscreened={fullscreened}>
-                <VideoPlayer ref={videoRef} key={url} onTimeUpdate={onTimeUpdate}>
-                    <source src={url}/>
-                </VideoPlayer>
-                {hasCloseButton && !fullscreened && <CloseButton onClick={closeButtonFn}><X/></CloseButton>}
-            </VideoContainer>
-            <ControlsContainer>
-                <Controls>
-                    <Icon onClick={() => setIsPaused(!isPaused)}>
-                        {isPaused ? <Play/> : <Pause/>}
-                    </Icon>
-                    <TimeControls>
-                        <TimeText>{conversionFn(currentTime)} / {conversionFn(duration)}</TimeText>
-                        <TimeProgressBarContainer>
-                            <TimeProgressBar />
-                            <input
-                                type="range"
-                                min="0"
-                                max="100"
-                                value={(currentTime / duration) ?? 0}
-                                onChange={(e) => handleVideoProgress(e)}
-                            />
-                        </TimeProgressBarContainer>
-                    </TimeControls>
-                    <VolumeControls>
-                        <Icon>
-                            <VolumeSpeaker />
-                        </Icon>
-                    </VolumeControls>
-                    <Icon onClick={toggleFullScreen}>
-                        <FullScreen/>
-                    </Icon>
-                </Controls>
-            </ControlsContainer>
+        <Container key={url}>
+            <Plyr
+                source={{
+                    type: 'video',
+                    sources: [{src: url}]
+                }}
+                options={{
+                    controls: [
+                        'play-large',
+                        'play',
+                        'progress',
+                        'current-time',
+                        'mute',
+                        'volume',
+                        'fullscreen'
+                    ],
+                    ratio: '16:9',
+                    invertTime: false
+                }}
+            />
+            {hasCloseButton && <CloseButton onClick={closeButtonFn}><X/></CloseButton>}
         </Container>
     );
     
